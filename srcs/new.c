4a36906847df9ae1a35f49c632060bbf06655599
Alka <alka.sv54@gmail.com>
2022-03-21 13:32:00 +0400

    tried altering time

--- srcs/new.c
+++ srcs/new.c
@@ -6,7 +6,7 @@
 /*   By: asanthos <asanthos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 20:40:23 by asanthos          #+#    #+#             */
-/*   Updated: 2022/03/20 13:56:14 by asanthos         ###   ########.fr       */
+/*   Updated: 2022/03/21 02:02:29 by asanthos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void philo_think(t_mutex *mut)
 	struct timeval m;
 
 	gettimeofday(&m, NULL);
-	printf("%d philo %d is thinking\n", (m.tv_usec - mut->p_create), mut->i);
+	printf("%ld philo %d is thinking\n", (m.tv_usec - mut->p_create), mut->i);
 }
 
 void philo_sleep(t_mutex *mut)
@@ -25,7 +25,7 @@ void philo_sleep(t_mutex *mut)
 	struct timeval m;
 
 	gettimeofday(&m, NULL);
-	printf("%d philo %d is sleeping\n", (m.tv_usec - mut->p_create), mut->i);
+	printf("%ld philo %d is sleeping\n", (m.tv_usec - mut->p_create), mut->i);
 	usleep(ft_atoi(mut->av[4]));
 	philo_think(mut);
 }
@@ -39,7 +39,7 @@ void *eat(void *arg)
 	if (mut.flag == 0)
 	{
 		gettimeofday(&m, NULL);
-		mut.p_create = m.tv_usec;
+		mut.p_create = (m.tv_usec / 1000) + (m.tv_sec * 1000);
 	}
 	mut.flag = 1;
 	mut.k = mut.i + 1;
@@ -54,7 +54,7 @@ void *eat(void *arg)
 	pthread_mutex_lock(&mut.fork[mut.i]);
 	mut.philo_fork[mut.i] = 1;
 	gettimeofday(&m, NULL);
-	printf("%d philo %d picks up a fork\n", (m.tv_usec - mut.p_create), mut.i);
+	printf("%ld philo %d picks up a fork\n", (((m.tv_usec / 1000) + (m.tv_sec * 1000)) - mut.p_create), mut.i);
 	pthread_mutex_unlock(&mut.fork[mut.i]);
 	// philo_sleep(&mut);
 	// printf("%d is i\n", mut.i);
@@ -68,7 +68,7 @@ void *eat(void *arg)
 			mut.philo_fork[mut.k] = 1;
 			gettimeofday(&m, NULL);
 			mut.right_fork = m.tv_sec;
-			printf("%d philo %d picks up a fork\n", (m.tv_usec - mut.p_create), mut.i);
+			printf("%ld philo %d picks up a fork\n", (((m.tv_usec / 1000) + (m.tv_sec * 1000)) - mut.p_create), mut.i);
 			pthread_mutex_unlock(&mut.fork[mut.k]);
 		}
 		else if (mut.philo_fork[mut.j] == 0)
@@ -77,7 +77,7 @@ void *eat(void *arg)
 			mut.philo_fork[mut.j] = 1;
 			gettimeofday(&m, NULL);
 			mut.left_fork = m.tv_sec;
-			printf("%d philo %d picks up a fork\n", (m.tv_usec - mut.p_create), mut.i);
+			printf("%ld philo %d picks up a fork\n", (((m.tv_usec / 1000) + (m.tv_sec * 1000)) - mut.p_create), mut.i);
 			pthread_mutex_unlock(&mut.fork[mut.k]);
 		}
 	}
@@ -92,7 +92,7 @@ void *eat(void *arg)
 			mut.philo_fork[mut.k] = 1;
 			gettimeofday(&m, NULL);
 			mut.right_fork = m.tv_sec;
-			printf("%d philo %d picks up a fork\n", (m.tv_usec - mut.p_create), mut.i);
+			printf("%ld philo %d picks up a fork\n", (((m.tv_usec / 1000) + (m.tv_sec * 1000)) - mut.p_create), mut.i);
 			pthread_mutex_unlock(&mut.fork[mut.k]);
 		}
 		else
@@ -101,7 +101,7 @@ void *eat(void *arg)
 			mut.philo_fork[mut.j] = 1;
 			gettimeofday(&m, NULL);
 			mut.left_fork = m.tv_sec;
-			printf("%d philo %d picks up a fork\n", (m.tv_usec - mut.p_create), mut.i);
+			printf("%ld philo %d picks up a fork\n", (((m.tv_usec / 1000) + (m.tv_sec * 1000)) - mut.p_create), mut.i);
 			pthread_mutex_unlock(&mut.fork[mut.k]);
 		}
 	}
@@ -112,7 +112,7 @@ void *eat(void *arg)
 				pthread_mutex_lock(&mut.fork[mut.k]);
 				gettimeofday(&m, NULL);
 				mut.philo_fork[mut.k] = 1;
-				printf("%d philo %d picks up a fork\n", (m.tv_usec - mut.p_create), mut.i);
+				printf("%ld philo %d picks up a fork\n", (((m.tv_usec / 1000) + (m.tv_sec * 1000)) - mut.p_create), mut.i);
 				pthread_mutex_unlock(&mut.fork[mut.k]);
 			}
 			else if (mut.philo_fork[mut.j] == 0)
@@ -120,7 +120,7 @@ void *eat(void *arg)
 				pthread_mutex_lock(&mut.fork[mut.j]);
 				gettimeofday(&m, NULL);
 				mut.philo_fork[mut.j] = 1;
-				printf("%d philo %d picks up a fork\n", (m.tv_usec - mut.p_create), mut.i);
+				printf("%ld philo %d picks up a fork\n", (((m.tv_usec / 1000) + (m.tv_sec * 1000)) - mut.p_create), mut.i);
 				pthread_mutex_unlock(&mut.fork[mut.j]);
 			}
 		}
@@ -128,7 +128,7 @@ void *eat(void *arg)
 
 		//to eat
 		gettimeofday(&m, NULL);
-		printf("%d philo %d is eating\n", (m.tv_usec - mut.p_create), mut.i);
+		printf("%ld philo %d is eating\n", (((m.tv_usec / 1000) + (m.tv_sec * 1000)) - mut.p_create), mut.i);
 		usleep(ft_atoi(mut.av[3]));
 		mut.philo_fork[mut.k] = 0;
 		mut.philo_fork[mut.i] = 0;
